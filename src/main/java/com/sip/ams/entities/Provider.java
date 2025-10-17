3cce0eb723c5d2c56c40d86e1804a27b6618a22f
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-10-17 14:05:31 +0100

    ajout de l'entité Provider

---
+++ src/main/java/com/sip/ams/entities/Provider.java
@@ -0,0 +1,56 @@
+package com.sip.ams.entities;
+
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity
+public class Provider {
+	@Id  // cela veut dire c'est une clé primaire
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String address;
+
+	public Provider(String name, String email, String address) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.address = address;
+	}
+	
+	public Provider() {
+		super();
+	}
+	
+	@Override
+	public String toString() {
+		return "Provider [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}

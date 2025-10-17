445640d98b0bbad5430ed36b23cf78f2f4a34326
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-10-17 14:57:15 +0100

    delete et getById

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -3,7 +3,9 @@ package com.sip.ams.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +51,40 @@ public class ProviderController {
 	
 	public ResponseEntity<Provider> saveProvider(@RequestBody Provider p) {
 		return new ResponseEntity<>(this.providerService.saveProvider(p), HttpStatus.CREATED);
+	}
+	
+	@Operation(summary = "Recherche d'un provider par son id")
+	@ApiResponses(value = { 
+			
+			@ApiResponse(responseCode = "200", description = "Si provider est trouvé"),
+			@ApiResponse(responseCode = "404", description = "Provider inexistant") })
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Provider> getProviderById(@PathVariable int id) {
+		
+	 Optional<Provider> opt = this.providerService.getProviderById(id);
+
+		if (opt.isEmpty())
+			return ResponseEntity.notFound().build();
+		else
+			return new ResponseEntity<>(opt.get(), HttpStatus.OK); // code 200
+	}
+	
+	@Operation(summary = "Suppression d'un provider par son id")
+	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Si provider est trouvé puis supprimé"),
+			@ApiResponse(responseCode = "404", description = "Provider inexistant") })
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Provider> deleteProviderById(@PathVariable int id){
+		
+		Optional<Provider> opt = this.providerService.getProviderById(id);
+		
+		if (opt.isEmpty())
+			return ResponseEntity.notFound().build();  // code 404
+		else {
+			this.providerService.deleteProviderById(id);
+			return ResponseEntity.noContent().build();  // code 204
+		}
 	}
 
 }
